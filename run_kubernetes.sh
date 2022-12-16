c70d8efe9f1cb6286d4de9cbb7140131dcd3783b
tien <tien>
2022-12-16 11:09:02 +0700

    init source

---
+++ run_kubernetes.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath="admin/ml-api-microservice-project4"
+# Step 2
+# Run the Docker Hub container with kubernetes
+docker login
+kubectl run ml-api-microservice-project4 --image=$dockerpath\
+    --port=80 --labels app=ml-api-microservice-project4
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward ml-api-microservice-project4 8000:80
